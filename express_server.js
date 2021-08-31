cea8d7f90a6ba929f965f4f6a2bcc4deca28b39e
MavFeuille <mavis.feuille@gmail.com>
2021-08-30 20:02:13 -0400

    Add a POST route to receive the form submission and generate a random ShortURL with 6 alphanumeric characters

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -37,6 +39,25 @@ app.get("/urls/:shortURL", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
+const generateRandomString = function() {
+  let randomString = "";
+  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < 6; i++)
+    randomString += characters.charAt(Math.floor(Math.random() * characters.length));
+
+  return randomString;
+}
+
+generateRandomString();
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
